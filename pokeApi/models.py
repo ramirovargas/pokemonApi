53f7ebfc905b9ef111b97276e876a3b8c0b189e2
Ramiro Vargas <ramiro.vargas@masivian.com>
2020-08-14 11:23:50 -0500

    add pokemon model

--- pokeApi/models.py
+++ pokeApi/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pokemon(models.Model):
+    Id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    base_stats = models.IntegerField()
+    height = models.IntegerField()
+    weight = models.IntegerField()
+    evolutions = models.TextField(null=True)
